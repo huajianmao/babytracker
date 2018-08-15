6a08fca898e1e2f6f4db910db2dc93f64d7ea798
Huajian Mao <huajianmao@gmail.com>
2018-08-15 10:50:48 +0800

    Add timeline in index page

--- mp/pages/index/index.js
+++ mp/pages/index/index.js
@@ -1,17 +1,22 @@
 //index.js
 //获取应用实例
 const app = getApp()
-
+const events = {
+  feed: { name: "喝奶奶咯~", icon: "../../imgs/baby/bottle.png" },
+  poops: { name: "拉臭臭了！", icon: "../../imgs/baby/cream.png" },
+  pee: { name: "尿了个尿 :-)", icon: "../../imgs/baby/cream.png" },
+  rest: { name: "宝宝在休息宝宝在休息宝宝在休息宝宝在休息", icon: "../../imgs/baby/child.png" },
+}
 Page({
   data: {
     records: [
-      { date: "2018-01-22", icon: "../../imgs/baby/child.png", event: "", picture: "", note: "" },
-      { date: "2018-01-23", icon: "../../imgs/baby/cream.png", event: "", picture: "", note: "" },
-      { date: "2018-01-23", icon: "../../imgs/baby/bottle.png", event: "", picture: "", note: "" },
-      { date: "2018-01-23", icon: "../../imgs/baby/cream.png", event: "", picture: "", note: "" },
-      { date: "2018-01-23", icon: "../../imgs/baby/bottle.png", event: "", picture: "", note: "" },
-      { date: "2018-01-23", icon: "../../imgs/baby/cream.png", event: "", picture: "", note: "" },
-      { date: "2018-01-23", icon: "../../imgs/baby/bottle.png", event: "", picture: "", note: "" }
+      { year: "2018", date: "01-22", time: "02:33:25", event: events.rest, picture: "", note: "说了，说了！说了，说了！说了，说了！说了，说了！说了，说了！说了，说了！说了，说了！说了，说了！" },
+      { year: "2018", date: "01-23", time: "06:53:43", picture: "", note: "" },
+      { year: "2018", date: "01-23", time: "08:24:46", event: events.feed, picture: "", note: "" },
+      { year: "2018", date: "01-23", time: "10:33:22", event: events.pee, picture: "", note: "" },
+      { year: "2018", date: "01-23", time: "11:34:52", event: events.feed, picture: "", note: "" },
+      { year: "2018", date: "01-23", time: "13:46:22", event: events.poops, picture: "", note: "" },
+  { year: "2018", date: "01-23", time: "16:18:32", event: events.feed, picture: "", note: "" }
     ],
     userInfo: {},
     hasUserInfo: false,
@@ -21,6 +26,9 @@ Page({
   // FIXME:
   addActionTap: function() {
     console.log("Add action tapped!")
+    wx.navigateTo({
+      url: '../record/edit'
+    })
   },
   // default actions
   bindViewTap: function() {
