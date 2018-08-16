42b9dfce8aa8222093c4686827b1dd56059c6aa0
Huajian Mao <huajianmao@gmail.com>
2018-08-16 15:52:25 +0800

    New action add / remove work

--- mp/pages/index/index.js
+++ mp/pages/index/index.js
@@ -1,34 +1,118 @@
 //index.js
+
+const util = require('../../utils/util.js')
+
 //获取应用实例
 const app = getApp()
-const events = {
+const events = wx.getStorageSync('events') ||{
   feed: { name: "喝奶奶咯~", icon: "../../imgs/baby/bottle.png" },
   poops: { name: "拉臭臭了！", icon: "../../imgs/baby/cream.png" },
-  pee: { name: "尿了个尿 :-)", icon: "../../imgs/baby/cream.png" },
-  rest: { name: "宝宝在休息宝宝在休息宝宝在休息宝宝在休息", icon: "../../imgs/baby/child.png" },
+  pee: { name: "尿了个尿 :-)", icon: "../../imgs/baby/pee.png" },
+  rest: { name: "宝宝在休息", icon: "../../imgs/baby/child.png" },
+  unknown: { name: "暂时还不知道", icon: "../../imgs/unknown.png" },
 }
+
 Page({
   data: {
-    records: [
-      { year: "2018", date: "01-22", time: "02:33:25", event: events.rest, picture: "", note: "说了，说了！说了，说了！说了，说了！说了，说了！说了，说了！说了，说了！说了，说了！说了，说了！" },
-      { year: "2018", date: "01-23", time: "06:53:43", picture: "", note: "" },
-      { year: "2018", date: "01-23", time: "08:24:46", event: events.feed, picture: "", note: "" },
-      { year: "2018", date: "01-23", time: "10:33:22", event: events.pee, picture: "", note: "" },
-      { year: "2018", date: "01-23", time: "11:34:52", event: events.feed, picture: "", note: "" },
-      { year: "2018", date: "01-23", time: "13:46:22", event: events.poops, picture: "", note: "" },
-  { year: "2018", date: "01-23", time: "16:18:32", event: events.feed, picture: "", note: "" }
+    events: events,
+    records: wx.getStorageSync('records') || [
+      { year: "2018", day: "01-22", time: "02:33:25", event: events.rest, picture: "", note: "说了，说了！说了，说了！说了，说了！" },
+      { year: "2018", day: "01-23", time: "06:53:43", picture: "", note: "" },
+      { year: "2018", day: "01-23", time: "08:24:46", event: events.feed, picture: "", note: "" },
+      { year: "2018", day: "01-23", time: "10:33:22", event: events.pee, picture: "", note: "" },
+      { year: "2018", day: "01-23", time: "11:34:52", event: events.feed, picture: "", note: "" },
+      { year: "2018", day: "01-23", time: "13:46:22", event: events.poops, picture: "", note: "" },
+      { year: "2018", day: "01-23", time: "16:18:32", event: events.feed, picture: "", note: "" }
     ],
+    onAddingAction: true,
+    newAction: {},
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
   //事件处理函数
-  // FIXME:
-  addActionTap: function() {
-    console.log("Add action tapped!")
-    wx.navigateTo({
-      url: '../record/edit'
+  addActionTap: function (e) {
+    initNewAction()
+  },
+  setNewActionTime: function() {
+    var newAction = this.data.newAction
+
+    var now = new Date()
+    const yearDayTime = util.formatYearDayTime(now)
+    newAction.year = yearDayTime[0]
+    newAction.day = yearDayTime[1]
+    newAction.time = yearDayTime[2]
+    this.setData({
+      newAction: newAction
     })
+  },
+  setNewActionEvent: function(event) {
+    var newAction = this.data.newAction
+    newAction.event = event
+    this.setData({
+      newAction: newAction
+    })
+  },
+  setNewActionNote: function(note) {
+    var newAction = this.data.newAction
+    newAction.note = note
+    this.setData({
+      newAction: newAction
+    })
+  },
+  initNewAction: function() {
+    this.setNewActionTime()
+    this.setNewActionEvent({})
+    this.setNewActionNote("")
+  },
+  cancelAddingTap: function(e) {
+    this.setData({
+      newAction: {},
+      // onAddingAction: false,
+    })
+  },
+  checkAddingTap: function(e) {
+    this.setNewActionTime()
+    var records = this.data.records
+    records.unshift(this.data.newAction)
+    wx.setStorageSync('records', records)
+
+    this.setData({
+      records: records,
+      // onAddingAction: false,
+      newAction: {}
+    })
+  },
+  addingEventDirectlyTap: function(e) {
+    this.eventSelectTap(e)
+    this.checkAddingTap(e)
+  },
+  eventSelectTap: function(e) {
+    this.setNewActionEvent(e.currentTarget.dataset.event)
+  },
+  bindTextAreaBlur: function (e) {
+    var newAction = this.data.newAction
+    newAction.note = e.detail.value
+    this.setData({
+      newAction: newAction
+    })
+  },
+  recordChange: function(e) {
+    // const recordID = e.currentTarget.dataset.index
+    // wx.navigateTo({
+    //   url: '../record/edit?record=' + recordID,
+    // })
+  },
+  removeActionTap: function(e) {
+    const recordID = e.currentTarget.dataset.index
+    
+    var records = this.data.records
+    records.splice(recordID, 1);
+    this.setData({
+      records: records,
+      // onAddingAction: false,
+    })
+    wx.setStorageSync('records', records)
   },
   // default actions
   bindViewTap: function() {
--- mp/utils/util.js
+++ mp/utils/util.js
@@ -9,11 +9,25 @@ const formatTime = date => {
   return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
 }
 
+const formatYearDayTime = date => {
+  const year = date.getFullYear()
+  const month = date.getMonth() + 1
+  const day = date.getDate()
+  const hour = date.getHours()
+  const minute = date.getMinutes()
+  const second = date.getSeconds()
+
+  const formated = [month, day, hour, minute, second].map(n => {return ('0' + n).slice(-2)})
+
+  return [year, formated.slice(0, 2).join('-'), formated.slice(2, 5).join(':')]
+}
+
 const formatNumber = n => {
   n = n.toString()
   return n[1] ? n : '0' + n
 }
 
 module.exports = {
-  formatTime: formatTime
+  formatTime: formatTime,
+  formatYearDayTime: formatYearDayTime
 }
