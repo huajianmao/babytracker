868dcd65a82c6673aa05332a04a8f2b51eb92173
Huajian Mao <huajianmao@gmail.com>
2019-02-28 09:27:26 +0800

    Add profile view

--- flutter/lib/main.dart
+++ flutter/lib/main.dart
@@ -23,12 +23,15 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  double _imageHeight = 256.0;
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       body: new Stack(
         children: <Widget>[
-          _buildIamge()
+          _buildIamge(),
+          _buildTopHeader(),
+          _buildProfileRow(),
         ],
       ),
     );
@@ -42,6 +45,68 @@ class _MainPageState extends State<MainPage> {
         fit: BoxFit.fitWidth,
         colorBlendMode: BlendMode.srcOver,
         color: new Color.fromARGB(120, 20, 10, 40),
+      ),
+    );
+  }
+
+  Widget _buildTopHeader() {
+    return new Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 32.0),
+      child: new Row(
+        children: <Widget>[
+          new Icon(Icons.menu, size: 32.0, color: Colors.white),
+          new Expanded(
+            child: new Padding(
+              padding: const EdgeInsets.only(left: 16.0),
+              child: new Text(
+                "Events",
+                style: new TextStyle(
+                    fontSize: 20.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.w300),
+              ),
+            ),
+          ),
+          new Icon(Icons.linear_scale, color: Colors.white),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildProfileRow() {
+    return new Padding(
+      padding: new EdgeInsets.only(left: 16.0, top: _imageHeight / 2.5),
+      child: new Row(
+        children: <Widget>[
+          new CircleAvatar(
+            minRadius: 28.0,
+            maxRadius: 28.0,
+            backgroundImage: new AssetImage('images/avatar.jpg'),
+          ),
+          new Padding(
+            padding: const EdgeInsets.only(left: 16.0),
+            child: new Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                new Text(
+                  '毛豆豆',
+                  style: new TextStyle(
+                      fontSize: 26.0,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w400),
+                ),
+                new Text(
+                  '2018-10-31',
+                  style: new TextStyle(
+                      fontSize: 14.0,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w300),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
